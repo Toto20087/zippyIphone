cec92a5a831740c9adfa0e21e4ad233c6378992a
Tomas Rodriguez Moyano <110465901+Toto20087@users.noreply.github.com>
2024-09-16 14:26:23 -0300

    Actualizacion del fodno

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,14 +36,9 @@ const Hero = () => {
     }, [])
 
   return (
-    <section className="w-full nav-height bg-black relative">
-        <div className="h-5/6 w-full flex-center flex-col">
-            <p id="hero" className="hero-title">iPhone 15 Pro</p>
-            <div className="md:w-10/12 w-9/12">
-                <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
-                    <source src={videoSrc} type="video/mp4" />
-                </video>
-            </div>
+    <section className='w-full nav-height relative bg-[#0099FF] bg-[url("/deskback.svg")] bg-no-repeat bg-center bg-cover '>
+        <div className="h-5/6 w-full flex-center flex-col pb-2">
+            <p id="hero" className="hero-title">Hacemos que enseñar tecnologia sea simple.</p>
         </div>
         <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
             <a href="#highlights" className="btn">Buy</a>
