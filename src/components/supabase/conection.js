2f9d0bc9b8a902f328168785ca4cdc2b15d94848
Tomas Rodriguez Moyano <110465901+Toto20087@users.noreply.github.com>
2024-10-30 13:37:41 -0300

    Update conection.js

--- src/components/supabase/conection.js
+++ src/components/supabase/conection.js
@@ -1,7 +1,8 @@
 // supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://zmbguwvkcnfpdrqyovqr.supabase.co";
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InptYmd1d3ZrY25mcGRycXlvdnFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc0NTY4ODAsImV4cCI6MjA0MzAzMjg4MH0.7twv8FeC_wGh9yUEKKHz4doH_2S_hattKBHI4Tha5XA';
+// Acceder a las variables de entorno
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
