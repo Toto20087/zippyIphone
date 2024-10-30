8a56cfd26d8aff9ec074132c240d423882789638
Tomas Rodriguez Moyano <110465901+Toto20087@users.noreply.github.com>
2024-10-30 13:58:20 -0300

    arreglos del redirect

--- src/components/ButtonGrande.jsx
+++ src/components/ButtonGrande.jsx
@@ -1,11 +1,20 @@
-const ButtonGrande = ({url, text}) => {
-    return (
-      <div 
+import { useNavigate } from "react-router-dom";
+
+const ButtonGrande = ({ url, text }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`${url}`);
+  };
+
+  return (
+    <div
       className="flex text-center items-center justify-center bg-[#33FFD1] cursor-pointer rounded-3xl pt-3 pb-3 pr-16 pl-16 text-lg mr-2 text-[#0099FF] transition-colors duration-200 hover:bg-[#BDFF00]"
-      >
-          <a className="decoration-transparent font-semibold" href={url}>{text}</a>
-      </div>
-    )
-  }
-  
-export default ButtonGrande
+      onClick={handleClick} // Cambiado aquí para pasar la referencia
+    >
+      <p className="decoration-transparent font-semibold">{text}</p>
+    </div>
+  );
+};
+
+export default ButtonGrande;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,19 @@
 import ButtonP from "./ButtonPropio"
+import { useNavigate } from "react-router-dom"
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/login")
+  }
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center bg-[#0099FF] ">
         <nav className="flex w-full justify-between">
             <img src="/logo.svg" alt="Zippy" width={80} height={80} />
-            <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
-              <a href="/login">
+            <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1" onClick={handleClick}>
                 <ButtonP 
                 text="Ingresa"
                 />
-              </a>
             </div>
         </nav>
     </header>
